ce84988b63a594739e5a32f4f1221e6425a40f69
dakong <dakong94@gmail.com>
2016-10-23 13:57:32 -0700

    added scss compilation, css loaders, file loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 //TODO add Hot Reloading, or just make add a watcher
 const path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const validate = require('webpack-validator');
 const PATHS = {
   app: path.join(__dirname, 'src'),
   build : path.join(__dirname, 'public/js')
 };
+
 
 const config = {
   entry: {
@@ -18,17 +21,29 @@ const config = {
     extensions:['','.js','.jsx']
   },
   module:{
-    loaders: [{
-      test: /\.jsx?$/,
-      //Cached for better performance
-      //Later we could make the cache directory to a custom place
-      //I.e., babel?cacheDirectory=<path>
-      loaders:['babel?cacheDirectory'],
-      //Parse only our src files. Should at our src directory!
-      include: PATHS.src
-    }
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders:['babel?cacheDirectory'],
+        include: PATHS.src
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style","css!sass"),
+        include: PATHS.src
+      }
     ]
-  }
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: 'src/static/index.html', to: '../'},
+      { from: 'src/static/404.html', to: '../'},
+      { from: 'src/static/app.html', to: '../'},
+      { from: 'src/static/home.html', to: '../'},
+      { from: 'src/images/bucket-logo.png', to: '../images'}
+    ]),
+    new ExtractTextPlugin('../styles/main.css')
+  ]
 };
 
 module.exports = config;
