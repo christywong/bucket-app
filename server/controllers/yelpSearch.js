ac705ad995374ca48ec9e7fefc1d6d0eb66bdd3d
dakong <dakong94@gmail.com>
2016-10-27 13:40:35 -0700

    Hot fix: Add href to links in app page

--- server/controllers/yelpSearch.js
+++ server/controllers/yelpSearch.js
@@ -12,16 +12,10 @@ if (process.env.LOCAL === 'true'){
   TOKEN           = config.get('token');
   TOKEN_SECRET    = config.get('token_secret');
 } else {
-  console.log('TESTING KEYS ON HEROKU!!!!');
   CONSUMER_KEY    = process.env.CONSUMER_KEY;
   CONSUMER_SECRET = process.env.CONSUMER_SECRET;
   TOKEN           = process.env.TOKEN;
   TOKEN_SECRET    = process.env.TOKEN_SECRET;
-
-  console.log(CONSUMER_KEY);
-  console.log(CONSUMER_SECRET);
-  console.log(TOKEN);
-  console.log(TOKEN_SECRET);
 }
 
 var yelp = new Yelp({
