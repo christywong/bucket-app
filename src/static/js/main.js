9cfdd6d40d594087231258418548d80bd59b5b3f
Joey Wong <joey.wong23@yahoo.com>
2016-10-31 17:36:23 -0700

    login changes

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -13,5 +13,30 @@
     event.preventDefault();
     window.location = '/app';
   });
+  check();
 
 })();
+
+(function check(){/*function to check valid username & password*/     
+  {
+    if (form.username.value ==""){
+     alert("Required field");
+      form.password.focus();
+      return false;
+        }
+    if (form.password.value ==""):
+      alert("Required field";
+        form.password.focus();
+        return false;
+        }
+    if (form.username.value == "user" && form.password.value == "pass")
+      window.location="/app";/*successfully logins into home page if login info matches */
+      }
+      else
+      {
+        alert("Invalid Password or Username") /* informs user of invalid login info*/
+      });
+
+
+
+
