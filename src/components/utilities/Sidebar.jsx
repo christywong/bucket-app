b9e987d83d341d50e1c496de1f440d931abad4aa
Christy Wong <cww016@ucsd.edu>
2016-11-10 17:45:39 -0800

    Took out delete icon from Archive and from All & Archive in mobile sidebar

--- src/components/utilities/Sidebar.jsx
+++ src/components/utilities/Sidebar.jsx
@@ -20,8 +20,12 @@ export default class Sidebar extends React.Component{
   }
 
   render(){
-    var list = this.props.bucketList;
+    const list = this.props.bucketList ? this.props.bucketList : [];
+    var createdBuckets = list.filter((bucket)=>(bucket.typeOfBucket !== 1 && bucket.typeOfBucket !== 2));
+    var allBucket = list.filter((bucket)=>(bucket.typeOfBucket === 1))[0];
+    var archiveBucket = list.filter((bucket)=>(bucket.typeOfBucket === 2))[0];
 
+    console.log('allBucket is: ', allBucket);
     const deletePopover = (
       <Popover id="popover-trigger-click-root-close" title="Are you sure?">
         <Button bsStyle="danger" bsSize="small" onClick = {()=>{
@@ -37,13 +41,13 @@ export default class Sidebar extends React.Component{
     return(
       <div className='sidebar'>
         <Button id="create-bucket-button" onClick={this.props.showBucketModal}>Create a Bucket</Button>
-        {list.map ( (bucket) => { return(
+        {createdBuckets.map ( (bucket) => { return(
           <Bucket changeStateBucket = {this.props.changeStateBucket}
             key = {bucket.id}
             bucketId = {bucket.id}
             bucketName = {bucket.title}
             active = {this.props.selectedBucket === bucket.id ? "active" : null}
-            showDeleteIcon = {this.props.selectedBucket === bucket.id && bucket.typeOfBucket !== 1 ? (
+            showDeleteIcon = {this.props.selectedBucket === bucket.id && bucket.typeOfBucket !== 1 && bucket.typeOfBucket !== 2 ? (
               <OverlayTrigger ref="deleteOverlay" trigger="click" rootClose placement="top" overlay={deletePopover}>
                 <i className="fa fa-trash-o" aria-hidden="true" id="delete-bucket-icon"></i>
               </OverlayTrigger>
